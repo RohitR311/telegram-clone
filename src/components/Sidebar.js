d96699bba6ec75b07d7fc3d73df075291fb96a3d
Rohit <rohit.rajan031101@gmail.com>
2020-11-26 19:47:57 +0530

    Initial Commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Sidebar.css";
+
+const SideBar = () => {
+  return <div className="sidebar">Sidebar</div>;
+};
+
+export default SideBar;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjlbtefBmzkrkI3jQEX055r2P7oh9rBYM",
+  authDomain: "telegram-clone-25ae6.firebaseapp.com",
+  databaseURL: "https://telegram-clone-25ae6.firebaseio.com",
+  projectId: "telegram-clone-25ae6",
+  storageBucket: "telegram-clone-25ae6.appspot.com",
+  messagingSenderId: "58910038962",
+  appId: "1:58910038962:web:a19603e870846ad958a2ab",
+};
